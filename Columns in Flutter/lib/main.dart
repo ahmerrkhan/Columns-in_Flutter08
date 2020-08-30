4233f5a62ec25aeeedcd5c23db2336c8e65f1567
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2020-08-30 15:44:59 +0500

    Columns in Flutter
    Basics

---
+++ Columns in Flutter/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String text = "Flat Button presses";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Columns in Flutter "),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(
+                onPressed: () {
+                  print("Flat Button 1");
+                },
+                color: Colors.blue,
+                child: Text("Flat 1")),
+            FlatButton(
+                onPressed: () {
+                  print("Flat Button 2");
+                },
+                color: Colors.green,
+                child: Text("Flat 2")),
+            FlatButton(
+                onPressed: () {
+                  print("Flat Button 3");
+                },
+                color: Colors.purple,
+                child: Text("Flat 3")),
+          ],
+        ),
+      ),
+    );
+  }
+}
